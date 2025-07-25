b556a4aa4b18c08da6218c6e7fe8a49e895205a5
Nazeer Shaikh <nazeer.shaikh54@gmail.com>
2025-07-25 00:57:27 -0400

    lesson 2

--- lesson_1/blank.py
+++ lesson_1/blank.py
@@ -1,3 +1,9 @@
-"""
-This is a blank notebook for py110
-"""
+def less_than(upper_limit):
+    numbers = []
+
+    for candidate in range(1, upper_limit):
+        numbers.append(candidate)
+
+    return numbers
+
+print(less_than(5))
